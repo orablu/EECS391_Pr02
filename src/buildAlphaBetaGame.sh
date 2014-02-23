963e101c24bb5e67c083f7c51d4592b066c40669
jxb532 <jxb532@case.edu>
2014-02-23 14:18:15 -0500

    added other classes to build script

--- src/buildAlphaBetaGame.sh
+++ src/buildAlphaBetaGame.sh
@@ -1,2 +1,2 @@
-javac -cp "Sepia.jar" AlphaBetaAgent.java
+javac -cp "Sepia.jar" AlphaBetaAgent.java AlphaBetaNode.java State.java FootmanAlphaBetaNode.java Node.java AlphaBetaLeaf.java
 java -cp "Sepia.jar;." edu.cwru.sepia.Main2 GameConfig.xml
