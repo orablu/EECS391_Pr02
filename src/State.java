fb0115a2097f375894223964e3c1ae06198e7264
Wes Rupert <ora@outlook.com>
2014-02-23 16:11:30 -0500

    Add board size to state

--- src/State.java
+++ src/State.java
@@ -1,15 +1,26 @@
 public class State {
-    public static final int ID = 0;
-    public static final int X = 1;
-    public static final int Y = 2;
+    public static final int X = 0;
+    public static final int Y = 1;
+    public static final int ID = 2;
     public static final int HP = 3;
+
+    public static int[] Min, Max;
+    public static void setupBoard(int minX, int maxX, int minY, int maxY) {
+        Min = new int[2]; Min[X] = minX; Min[Y] = minY;
+        Max = new int[2]; Max[X] = maxX; Max[Y] = maxY;
+    }
+    
+    public static int[] createEntity(int id, int hp, int x, int y) {
+        int[] entity = new int[4];
+        entity[ID] = id;
+        entity[HP] = hp;
+        entity[X] = x;
+        entity[Y] = y;
+        return entity;
+    }
 
     private int[][] footmen;
     private int[][] archers;
-
-    public static int[] createEntity(int id, int hp, int x, int y) {
-        return new int[] { id, x, y, hp };
-    }
 
     public State() {
         this(
