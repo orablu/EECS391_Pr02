386142ce5eb9c01e1da605bac700f5c36b80663f
jxb532 <jxb532@case.edu>
2014-02-23 19:12:16 -0500

    updated build script

--- src/buildAlphaBetaGame.sh
+++ src/buildAlphaBetaGame.sh
@@ -1,2 +1,2 @@
-javac -cp "Sepia.jar" AlphaBetaAgent.java AlphaBetaNode.java State.java FootmanAlphaBetaNode.java Node.java AlphaBetaLeaf.java
+javac -cp "Sepia.jar" AlphaBetaAgent.java Node.java Unit.java State.java Action.java AlphaBetaLeaf.java AlphaBetaNode.java FootmanAlphaBetaNode.java FootmanAlphaBetaLeaf.java ArcherAlphaBetaNode.java ArcherAlphaBetaLeaf.java
 java -cp "Sepia.jar;." edu.cwru.sepia.Main2 GameConfig.xml
